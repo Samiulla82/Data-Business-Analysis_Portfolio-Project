a9da8208e91d4b49910ab6a1aea226e0ba961f65
Sami <43133400+Samiulla82@users.noreply.github.com>
2024-06-28 10:57:33 +0530

    Update Olympics-Mini Case study.sql

--- SQL Projects/Olympics-Mini Case study/Olympics-Mini Case study.sql
+++ SQL Projects/Olympics-Mini Case study/Olympics-Mini Case study.sql
@@ -5,9 +5,8 @@
 
   SELECT * FROM olympics
 
-  ReName Table name 
- 
- sp_rename 'olympics_cleaned_v4',  'olympics';
+  --Rename Tables name--- 
+  sp_rename 'olympics_cleaned_v4',  'olympics';
 
 --Problem 1
 --Display the names of athletes who won a gold medal in the 2008 Olympics and whose height is greater than the average height of all athletes in the 2008 Olympics.
@@ -15,23 +14,21 @@
 SELECT *FROM olympics
 WHERE Medal = 'Gold' AND Year = 2008 AND Height> (SELECT AVG(Height) 
                                                     FROM olympics 
-													WHERE Year = 2008)
+						     WHERE Year = 2008)
 
 
 --Problem 2
 --Display the names of athletes who won a medal in the sport of basketball in the 2016 Olympics and whose weight is less than the average weight of all athletes who won a medal in the 2016 Olympics.
 
-
 SELECT * FROM olympics
 WHERE Sport= 'Basketball' AND Year= 2016 AND Weight < (SELECT AVG(Weight) FROM olympics WHERE Year= 2016) AND Medal IS NOT NULL
+
 
 --Problem 3
 --Display the names of all athletes who have won a medal in the sport of swimming in both the 2008 and 2016 Olympics.
 
 SELECT * FROM olympics
 WHERE Sport= 'Swimming'  AND Year IN (2008,2016) AND Medal IS NOT NULL
-
-
 
 
 --Problem 4  
@@ -43,11 +40,8 @@ HAVING Count(Medal) > 50
 ORDER BY Year,Num_Medals DESC
 
 
-
-
 --Problem 5
 --Display the names of all athletes who have won medals in more than one sport in the same year.
-
 
 SELECT Name,Year,Sport AS Sports, Count(Event)AS Events, COUNT(Medal) AS Medals FROM Olympics
 GROUP BY Name,year, Sport
@@ -58,15 +52,13 @@ ORDER BY Year ASC
 --Problem 6
 --What is the average weight difference between male and female athletes in the Olympics who have won a medal in the same event?
 
-
 SELECT 
     AVG(abs(male.weight - female.weight)) AS avg_weight_difference
 FROM 
     Olympics  AS male
     JOIN Olympics  AS female ON male.event = female.event
-WHERE 
-    male.sex = 'M' 
-    AND female.sex = 'F'
-    AND male.medal IS NOT NULL
-    AND female.medal IS NOT NULL;
+     WHERE  male.sex = 'M' 
+     AND female.sex = 'F'
+     AND male.medal IS NOT NULL
+     AND female.medal IS NOT NULL;
 
