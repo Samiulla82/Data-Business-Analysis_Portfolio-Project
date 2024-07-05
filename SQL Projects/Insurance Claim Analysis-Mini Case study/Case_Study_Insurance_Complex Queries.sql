3fecd230269960dbc71fc1c278d1c2e1f5578a3d
Sami <43133400+Samiulla82@users.noreply.github.com>
2024-07-05 12:42:31 +0530

    Update Case_Study_Insurance_Complex Queries.sql

--- SQL Projects/Insurance Claim Analysis-Mini Case study/Case_Study_Insurance_Complex Queries.sql
+++ SQL Projects/Insurance Claim Analysis-Mini Case study/Case_Study_Insurance_Complex Queries.sql
@@ -15,6 +15,8 @@ WHERE
 	Highest_Claimer <= 5;
 
 
+
+
 --Problem 2: What is the average insurance claimed by patients based on the number of children they have?
 
 SELECT 
@@ -26,8 +28,9 @@ FROM
 ORDER BY 
     children;
 
---Problem 3: What is the highest and lowest claimed amount by patients in each region?
 
+
+--Problem 3: What is the highest and lowest claimed amount by patients in each region?
 
 SELECT  *,
 	MIN(claim) OVER(PARTITION BY region) AS Min_Claim_Reg,
@@ -58,7 +61,6 @@ FROM insurance_data
 
 
 
-
 --Problem 5: What is the difference between the claimed amount of each patient and the first claimed amount of that patient?
 
 SELECT 
@@ -72,6 +74,8 @@ ORDER BY
     PatientID
 
 
+
+	
 --Problem 6: For each patient, calculate the difference between their claimed amount and the average claimed amount of patients with the same number of children.
 
 WITH AverageClaimed AS(
@@ -106,10 +110,7 @@ ORDER BY region
 
 
 
-
-
 --Problem 8: Calculate the difference between the claimed amount of each patient and the claimed amount of the patient who has the highest BMI in their region.
-
 
 WITH MaxBmiPatients AS (
     SELECT 
@@ -136,6 +137,8 @@ ON
 ORDER BY 
     i.PatientID;
 
+
+
 --Problem 9: For each patient, calculate the difference in claim amount between the patient and the patient with the highest claim amount among patients with the same bmi and smoker status, within the same region. Return the result in descending order difference.
 
 SELECT *,
@@ -144,12 +147,8 @@ ROUND((FIRST_VALUE(claim) OVER(PARTITION BY region, smoker,bmi ORDER BY bmi ASC 
 FROM insurance_data
 
 
-
-
-
-
+	
 --Problem 10: For each patient, find the maximum BMI value among their next three records (ordered by age).
-
 
 SELECT 
     PatientID,
@@ -158,6 +157,7 @@ SELECT
     MAX(bmi) OVER(PARTITION BY PatientID ORDER BY age ASC ROWS BETWEEN 1 FOLLOWING AND 3 FOLLOWING) AS Max_BMI_Next_3_Records
 FROM 
     insurance_data;
+
 
 
 
